939d8fb110ae917c312cf1537bc6380434f6b4c1
JinKun11605 <jinkun.116.05@gmail.com>
2024-10-26 02:09:00 +0700

    Interchange Sort

--- public/js/interchangesort.js
+++ public/js/interchangesort.js
@@ -13,31 +13,30 @@ document.addEventListener('DOMContentLoaded', async function () {
         const n = arr.length;
         for (let i = 0; i < n - 1; i++) {
             let swapped = false;
-            for (let j = 0; j < n - i - 1; j++) {
+            for (let j = i+ 1; j < n; j++) {
+                blocks[i].style.backgroundColor = 'red';
                 blocks[j].style.backgroundColor = 'red';
-                blocks[j + 1].style.backgroundColor = 'red';
 
                 // * Gọi hàm pause từ swap-blocks.js
                 await pause(500);
 
-                if (arr[j] > arr[j + 1]) {
+                if (arr[i] > arr[j]) {
                     // Đổi chỗ hai phần tử trong mảng
-                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                    [arr[i], arr[j]] = [arr[j], arr[i]];
 
                     // * Gọi hàm swapBlocks từ swap-blocks.js
-                    await swapBlocks(blocks[j], blocks[j + 1]);
+                    await swapBlocks(blocks[i], blocks[j]);
 
                     swapped = true;
                 }
 
+                blocks[i].style.backgroundColor = '#7FC5FC';
                 blocks[j].style.backgroundColor = '#7FC5FC';
-                blocks[j + 1].style.backgroundColor = '#7FC5FC';
             }
-            blocks[n - i - 1].style.backgroundColor = 'green';
+            blocks[i].style.backgroundColor = 'green';
 
             if (!swapped) break; // Nếu không có hoán đổi nào, dừng thuật toán
         }
-        //test
         // Đánh dấu các khối còn lại là đã sắp xếp
         for (let k = 0; k < n; k++) {
             blocks[k].style.backgroundColor = 'green';
@@ -45,7 +44,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
 
     // Nút "Bắt Đầu Sắp Xếp"
-    sortButton.addEventListener('click', () => { {
+    sortButton.addEventListener('click', () => {
         bubbleSort(numbersArray);
     });
 });
