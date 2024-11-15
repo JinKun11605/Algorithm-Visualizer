01ffb0afd70a0213ff3889cc39bfeac6854c2a21
JinKun11605 <jinkun.116.05@gmail.com>
2024-11-16 03:23:44 +0700

    QuickSort 98% - Còn lỗi màu 1 số trường hợp

--- public/js/quicksort.js
+++ public/js/quicksort.js
@@ -16,31 +16,29 @@ const quickSort = async (arr, left, right) => {
     blocks[pivotIndex].style.backgroundColor = "#E94345"; // Đỏ
     await pause(500);
 
-    while (i < j) {
+    while (i <= j) {
         // Bật màu phần tử đang xét từ trái
         while (arr[i] < pivot) {
             blocks[i].style.backgroundColor = "#3E97CF";
             await pause(500);
             blocks[i].style.backgroundColor = "#5C636A";
-
             i++;
         }
 
         // Bật màu phần tử đang xét từ phải
-        while (arr[j] > pivot) {
+        while (arr[j] > pivot){
             blocks[j].style.backgroundColor = "#3E97CF";
             await pause(500);
             blocks[j].style.backgroundColor = "#5C636A";
-
             j--;
         }
         
 
         if (i <= j) {
-            if (i !== j && arr[i] != arr[j]) {
-                blocks[i].style.backgroundColor = "#FFE32F";
-                blocks[j].style.backgroundColor = "#FFE32F";
-
+            if (i !== j && arr[i] !== arr[j]) {
+                blocks[i].style.backgroundColor = "#FFE32F"; // Vàng
+                blocks[j].style.backgroundColor = "#FFE32F"; // Vàng
+                
                 await pause(500);
                 await swapBlocks(blocks[i], blocks[j]);
                 [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -48,22 +46,23 @@ const quickSort = async (arr, left, right) => {
                 blocks[i].style.backgroundColor = "#5C636A";
                 blocks[j].style.backgroundColor = "#5C636A";
             }
-
             i++;
             j--;
         }
     }
-
-    // Tắt màu pivot
+    
+    // Tắt màu pivot sau khi phân tách xong
     blocks[pivotIndex].style.backgroundColor = "#5C636A";
 
+    // Đệ quy sắp xếp các mảng con
     if (left < j) {
         await quickSort(arr, left, j);
     }
     if (i < right) {
         await quickSort(arr, i, right);
     }
-    // Đánh dấu phần đã được sắp xếp
+
+    // Đánh dấu các phần tử đã được sắp xếp
     for (let k = left; k <= right; k++) {
         blocks[k].style.backgroundColor = "#4DBE8A";
     }
