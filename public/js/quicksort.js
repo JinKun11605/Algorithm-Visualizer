1025b7ed6e13b1423bf346b06680d2fa8b4fea35
JinKun11605 <jinkun.116.05@gmail.com>
2024-11-16 02:45:45 +0700

    QuickSort 90%

---
+++ public/js/quicksort.js
@@ -0,0 +1,76 @@
+import { centerBlocks } from "../helpers/position-blocks.js";
+import { pause, swapBlocks } from "../helpers/swap-blocks.js";
+
+const blocks = Array.from(document.querySelectorAll(".block"));
+const numbersArray = blocks.map(block => parseInt(block.textContent));
+
+// * Gọi hàm centerBlocks từ position-blocks.js
+centerBlocks("visualization", blocks, 20);
+
+const quickSort = async (arr, left, right) => {
+    let i = left, j = right;
+    const pivotIndex = Math.floor((left + right) / 2);
+    const pivot = arr[pivotIndex];
+
+    // Bật màu pivot
+    blocks[pivotIndex].style.backgroundColor = "#E94345"; // Đỏ
+    await pause(500);
+
+    while (i < j) {
+        // Bật màu phần tử đang xét từ trái
+        while (arr[i] < pivot) {
+            blocks[i].style.backgroundColor = "#3E97CF";
+            await pause(500);
+            blocks[i].style.backgroundColor = "#5C636A";
+
+            i++;
+        }
+
+        // Bật màu phần tử đang xét từ phải
+        while (arr[j] > pivot) {
+            blocks[j].style.backgroundColor = "#3E97CF";
+            await pause(500);
+            blocks[j].style.backgroundColor = "#5C636A";
+
+            j--;
+        }
+        
+
+        if (i <= j) {
+            if (i !== j && arr[i] != arr[j]) {
+                blocks[i].style.backgroundColor = "#FFE32F";
+                blocks[j].style.backgroundColor = "#FFE32F";
+
+                await pause(500);
+                await swapBlocks(blocks[i], blocks[j]);
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+
+                blocks[i].style.backgroundColor = "#5C636A";
+                blocks[j].style.backgroundColor = "#5C636A";
+            }
+
+            i++;
+            j--;
+        }
+    }
+
+    // Tắt màu pivot
+    blocks[pivotIndex].style.backgroundColor = "#5C636A";
+
+    if (left < j) {
+        await quickSort(arr, left, j);
+    }
+    if (i < right) {
+        await quickSort(arr, i, right);
+    }
+    // Đánh dấu phần đã được sắp xếp
+    for (let k = left; k <= right; k++) {
+        blocks[k].style.backgroundColor = "#4DBE8A";
+    }
+}
+
+// Nút "Bắt Đầu Sắp Xếp"
+const sortButton = document.querySelector("#sort-button");
+sortButton.addEventListener("click", () => {
+    quickSort(numbersArray, 0, numbersArray.length - 1);
+})
