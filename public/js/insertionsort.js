9f0396bc56ccda6a5e94be44916f0ace20758b23
JinKun11605 <jinkun.116.05@gmail.com>
2024-11-19 20:11:15 +0700

    InsertionSort 100%

--- public/js/insertionsort.js
+++ public/js/insertionsort.js
@@ -10,23 +10,28 @@ centerBlocks("visualization", blocks, 20);
 const insertionSort = async (arr, blocks) => {
     const n = arr.length;
 
-    for (let i = 0; i < n; i++) { //! Bắt đầu từ 0 để đèn sáng từ đầu.
+    for (let i = 1; i < n; i++) {
+        blocks[0].style.backgroundColor = "#4DBE8A"; //* Bật màu cho block đầu tiên
         let index = i;
+
         blocks[index].style.backgroundColor = "#E94345";
-        await pause(500);
+        await pause(200);
 
         while (index > 0 && arr[index] < arr[index - 1]) {
             await pause(50);
-            await swapBlocks(blocks[index - 1], blocks[index]);
-
-            blocks[index].style.backgroundColor = "#5C636A";
-
+            await swapBlocks(blocks[index - 1], blocks[index], 200);
             [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
+            
+            blocks[index].style.backgroundColor = "#4DBE8A"; //! Swap xong thì block sau thì index => Đổi thành màu xanh
+
             index--;
 
             blocks[index].style.backgroundColor = "#E94345";
         }
-        blocks[index].style.backgroundColor = "#5C636A";
+        
+        for (let t = 0; t <= i; t++){
+            blocks[t].style.backgroundColor = "#4DBE8A";
+        }
     }
 
     for (let k = 0; k < n; k++) {
