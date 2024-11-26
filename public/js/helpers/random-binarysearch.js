cbc6fdf890900c85cd8e0ce0e98dc10077d1bf06
JinKun11605 <jinkun.116.05@gmail.com>
2024-11-26 16:14:48 +0700

    done

--- public/js/helpers/random-binarysearch.js
+++ public/js/helpers/random-binarysearch.js
@@ -2,7 +2,7 @@ const randomButton = document.querySelector("#random-button");
 
 randomButton.addEventListener("click", () => {
     const length = Math.ceil(Math.random() * 16);
-    const numbersArray = Array.from({ length: length }, () => Math.ceil(Math.random() * 16));
+    const numbersArray = Array.from({ length: length }, () => Math.ceil(Math.random() * 99));
     numbersArray.sort((a, b) => a - b);
 
     const numbersInput = document.querySelector("#numbers-input");
--- public/js/helpers/random-linearsearch.js
+++ public/js/helpers/random-linearsearch.js
@@ -2,7 +2,7 @@ const randomButton = document.querySelector("#random-button");
 
 randomButton.addEventListener("click", () => {
     const length = Math.ceil(Math.random() * 16);
-    const numbersArray = Array.from({ length: length }, () => Math.ceil(Math.random() * 16));
+    const numbersArray = Array.from({ length: length }, () => Math.ceil(Math.random() * 99));
     
     const numbersInput = document.querySelector("#numbers-input");
     numbersInput.value = numbersArray.join(", ");
